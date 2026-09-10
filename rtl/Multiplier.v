65566c90469d2e3e20562c6eb9343b2e73d1db2a
YuBin <yubinchew665@gmail.com>
2026-09-11 01:28:28 +0800

    Update Multiplier.v

--- rtl/Multiplier.v
+++ rtl/Multiplier.v
@@ -1,29 +1,8 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 18.08.2026 10:55:44
-// Design Name: 
-// Module Name: Multiplier
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 `define MUL 4'h0
 `define MULH 4'h1
 `define MULHSU 4'h2
 `define MULHU 4'h3
+
 
 module Multiplier(
     input [31:0] reg_rs_1,
@@ -36,10 +15,12 @@ module Multiplier(
     wire signed [63:0] mul_su; // Signed x Unsigned => Signed
     wire        [63:0] mul_uu; // Unsigned x Unsigned => Unsigned
 
+
     // Compute 64-bit intermediate products
     assign mul_ss = $signed(reg_rs_1) * $signed(reg_rs_2);
     assign mul_su = $signed({reg_rs_1[31], reg_rs_1}) * $signed({1'b0, reg_rs_2}); // Zero-extend rs2 to prevent sign treatment
     assign mul_uu = $unsigned(reg_rs_1) * $unsigned(reg_rs_2);
+
 
     always @(*) begin
         case (mult_sel)
@@ -49,5 +30,5 @@ module Multiplier(
             `MULHU: rd = mul_uu[63:32]; // MULHU: upper 32 bits (Unsigned * Unsigned)
             default: rd = 32'h0;
         endcase
-    end 
+    end
 endmodule
