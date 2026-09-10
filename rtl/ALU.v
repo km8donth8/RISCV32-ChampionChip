601f54f0e6141bf67efbc1505cd2125d98646150
YuBin <yubinchew665@gmail.com>
2026-09-11 01:20:32 +0800

    Update ALU.v

--- rtl/ALU.v
+++ rtl/ALU.v
@@ -1,29 +1,9 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02.08.2026 14:14:31
-// Design Name: 
-// Module Name: ALU
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 //NOTE: ALU control signals
 //funct7 [31:25] - lsb used
 //funct3 [14:12] - all 3 used
 //opcode [6:0] - fixed for R-type
 //funct7[25] + funct3[14:12]
+
 
 `define c_ALU_OP_PASS_B    4'h0
 `define c_ALU_OP_ADD    4'h1
@@ -37,6 +17,7 @@
 `define c_ALU_OP_SLT    4'h9
 `define c_ALU_OP_SLTU    4'hA
 
+
 module ALU(
     input wire [31:0] reg_rs_1,
     input wire [31:0] reg_rs_2,
@@ -49,11 +30,14 @@ module ALU(
     );
     wire signed [31:0] A_Mux;
 
+
     wire signed [31:0] B_Mux;
 
+
     assign A_Mux = (A_sel) ? pc_output : reg_rs_1;
-    
-    assign B_Mux = (B_sel) ? immediate : reg_rs_2; 
+   
+    assign B_Mux = (B_sel) ? immediate : reg_rs_2;
+
 
     always @ (*) begin
         case (ALU_control)
@@ -68,10 +52,7 @@ module ALU(
             `c_ALU_OP_MRS:    Q = A_Mux >>> B_Mux[4:0]; // Performs arithmetic right shift properly since A_Mux is signed
             `c_ALU_OP_SLT:    Q = (A_Mux < B_Mux) ? 32'h1 : 32'h0;
             `c_ALU_OP_SLTU:   Q = ($unsigned(A_Mux) < $unsigned(B_Mux)) ? 32'h1 : 32'h0; // Explicitly cast to unsigned
-            default:          Q = 32'h0; // Prevents unwanted latches
-            
+            default:          Q = 32'h0; // Prevents unwanted latches  
         endcase
     end
-
-
 endmodule
