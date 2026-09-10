b51038c32639438b79d90a8577c7d50ae620cf39
YuBin <yubinchew665@gmail.com>
2026-09-11 01:37:35 +0800

    Update tb_RegisterFile.v

--- tb/tb_RegisterFile.v
+++ tb/tb_RegisterFile.v
@@ -1,26 +1,7 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 21.08.2026 15:11:13
-// Design Name: 
-// Module Name: tb_RegisterFile
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 `timescale 1ns / 1ps
 
-module tb_RegisterFile;
+module testbench;
 
     // Parameters
     localparam DATA_MEM_SIZE = 8192; // 8 KB
