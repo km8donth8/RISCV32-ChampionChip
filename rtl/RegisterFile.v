2b05050e0baeb9fcf14a3c6b95302d655ec7253a
YuBin <yubinchew665@gmail.com>
2026-09-11 01:33:52 +0800

    Update RegisterFile.v

--- rtl/RegisterFile.v
+++ rtl/RegisterFile.v
@@ -1,25 +1,5 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03.08.2026 21:55:15
-// Design Name: 
-// Module Name: RegisterFile
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 module RegisterFile #(
-    parameter DATA_MEM_SIZE = 2**10
+    parameter DATA_MEM_SIZE = 2**13
 )(
     input  wire        clk,
     input  wire        rst_n,
@@ -30,21 +10,26 @@ module RegisterFile #(
     output wire [31:0] reg_rs_2_data
 );
 
+
     // Constants
     localparam SP_INDEX         = 5'd2;
     localparam GP_INDEX         = 5'd3;
     localparam GP_INITIAL_VALUE = 32'h1001_0000;
     localparam SP_INITIAL_VALUE = GP_INITIAL_VALUE + DATA_MEM_SIZE - 4;
 
+
     // Instruction field decoding
     wire [4:0] reg_rd_addr   = instruction[11:7];
     wire [4:0] reg_rs_1_addr = instruction[19:15];
     wire [4:0] reg_rs_2_addr = instruction[24:20];
 
+
     // 32 General-Purpose 32-bit Registers (x0 to x31)
     reg [31:0] regs [0:31];
 
+
     integer i;
+
 
     // --- SYNCHRONOUS WRITES & RESET ---
     always @(posedge clk or negedge rst_n) begin
@@ -64,9 +49,11 @@ module RegisterFile #(
         end
     end
 
+
     // --- ASYNCHRONOUS READS ---
     // General Purpose Register outputs (x0 strictly hardwired to 0)
     assign reg_rs_1_data = (reg_rs_1_addr == 5'b00000) ? 32'h0 : regs[reg_rs_1_addr];
     assign reg_rs_2_data = (reg_rs_2_addr == 5'b00000) ? 32'h0 : regs[reg_rs_2_addr];
+
 
 endmodule
